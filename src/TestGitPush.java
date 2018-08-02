dc17553326bccf418ee9177292546ff0957313e0
splitwater <ysinaq@gmail.com>
2018-08-02 21:31:29 +0900

    THIRD COMMIT

--- src/TestGitPush.java
+++ src/TestGitPush.java
@@ -4,7 +4,8 @@ public class TestGitPush {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,World");
-		System.out.println("Hey,Siri");
+		System.out.println("Hey, Siri");
+		System.out.println("OK, GOOGLE");
 
 	}
 
